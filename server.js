d19a90355749319ee182e2601e21cacd83c4fc05
JelleRomijn <jelletijmen@gmail.com>
2026-04-08 10:56:14 +0200

    Refactor styling into separate stylesheet and fix CSS MIME type

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const mimeTypes = {
+  '.html': 'text/html; charset=utf-8',
+  '.js': 'application/javascript; charset=utf-8',
+  '.css': 'text/css; charset=utf-8'
+};
+
 const server = http.createServer((req, res) => {
   let filePath = '.' + (req.url === '/' ? '/index.html' : req.url);
   let ext = path.extname(filePath);
-  let contentType = ext === '.js' ? 'application/javascript' : 'text/html';
+  let contentType = mimeTypes[ext] || 'application/octet-stream';
   fs.readFile(filePath, (err, data) => {
     if (err) { res.writeHead(404); res.end('Not found'); return; }
     res.writeHead(200, { 'Content-Type': contentType });
